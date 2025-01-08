7cd2f8f25c2cad3743bc067f08d7333f786e130c
Przemyslaw Piorkowski <piorkowskiprzemyslaw@gmail.com>
2025-01-08 22:46:42 +0100

    Tree exercises

--- src/main/scala/fpinscala/exercises/datastructures/Tree.scala
+++ src/main/scala/fpinscala/exercises/datastructures/Tree.scala
@@ -8,9 +8,14 @@ enum Tree[+A]:
     case Leaf(_) => 1
     case Branch(l, r) => 1 + l.size + r.size
 
-  def depth: Int = ???
+  def depth: Int = this match
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + (left.depth max right.depth)
+  
 
-  def map[B](f: A => B): Tree[B] = ???
+  def map[B](f: A => B): Tree[B] = this match
+    case Leaf(value) => Leaf(f(value))
+    case Branch(left, right) => Branch(left.map(f), right.map(f))
 
   def fold[B](f: A => B, g: (B,B) => B): B = ???
   
@@ -28,6 +33,8 @@ object Tree:
 
   extension (t: Tree[Int]) def firstPositive: Int = ???
 
-  extension (t: Tree[Int]) def maximum: Int = ???
+  extension (t: Tree[Int]) def maximum: Int = t match 
+    case Leaf(v) => v
+    case Branch(l, r) => l.maximum max r.maximum
 
   extension (t: Tree[Int]) def maximumViaFold: Int = ???
